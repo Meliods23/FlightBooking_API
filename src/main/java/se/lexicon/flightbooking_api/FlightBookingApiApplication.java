4b5e0bc75bdec30876eb936843c23425a5bd3927
loai-admin <loai.homs1970@bahnhof.se>
2025-08-04 12:43:51 +0200

    Feat: Add support for LLM tools/functions usage
    Allows LLM usage of custom functions, this grants access to direct interactions with Flights DB

--- src/main/java/se/lexicon/flightbooking_api/FlightBookingApiApplication.java
+++ src/main/java/se/lexicon/flightbooking_api/FlightBookingApiApplication.java
@@ -1,7 +1,11 @@
 package se.lexicon.flightbooking_api;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import se.lexicon.flightbooking_api.service.aiTools.FlightTools;
 
 @SpringBootApplication
 public class FlightBookingApiApplication {
@@ -10,4 +14,11 @@ public class FlightBookingApiApplication {
         SpringApplication.run(FlightBookingApiApplication.class, args);
     }
 
+    @Bean
+    public ChatClient chatClient(ChatModel chatModel, FlightTools flightTools) {
+        ChatClient.Builder builder = ChatClient.builder(chatModel);
+        builder.defaultTools(flightTools);
+
+        return builder.build();
+    }
 }
--- src/main/java/se/lexicon/flightbooking_api/service/aiTools/FlightTools.java
+++ src/main/java/se/lexicon/flightbooking_api/service/aiTools/FlightTools.java
@@ -1,4 +1,54 @@
 package se.lexicon.flightbooking_api.service.aiTools;
 
+import lombok.AllArgsConstructor;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Component;
+import se.lexicon.flightbooking_api.dto.AvailableFlightDTO;
+import se.lexicon.flightbooking_api.dto.BookFlightRequestDTO;
+import se.lexicon.flightbooking_api.dto.FlightBookingDTO;
+import se.lexicon.flightbooking_api.service.FlightBookingService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
 public class FlightTools {
+
+    FlightBookingService flightBookingService;
+
+    @Tool(description = "Get available flights")
+    public List<AvailableFlightDTO> findAvailableFlights() {
+        System.out.println("Getting available flights");
+        flightBookingService.findAvailableFlights().stream().forEach(f -> System.out.println(f.destination()));
+
+        return flightBookingService.findAvailableFlights();
+    }
+
+    @Tool(description = "Get available flights destinations")
+    public List<String> findAvailableFlightsDestinations() {
+        List<String> destinations = new ArrayList<>();
+        System.out.println("Getting available flights destinations");
+        flightBookingService.findAvailableFlights().stream().forEach(f -> destinations.add(f.destination()));
+
+        return destinations;
+    }
+
+    @Tool(description = "Find booked flights by email")
+    List<FlightBookingDTO> findBookingsByEmail(String email){
+        System.out.println("Getting booked flights by email");
+        // flightBookingService.findBookingsByEmail(email).stream().forEach(f -> System.out.println(f.destination()));
+        return flightBookingService.findBookingsByEmail(email);
+    }
+
+    @Tool(description = "Cancel a booked flight")
+    void cancelFlight(Long flightId, String passengerEmail){
+        System.out.println("Canceling booked flight");
+        flightBookingService.cancelFlight(flightId, passengerEmail);
+    }
+
+    @Tool(description = "Book a flight")
+    FlightBookingDTO bookFlight(Long flightId, BookFlightRequestDTO bookingRequest){
+        return flightBookingService.bookFlight(flightId, bookingRequest);
+    }
 }
